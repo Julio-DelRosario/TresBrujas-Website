a2f70799c9455d545ac5a33bbf208e0ad1ceb222
Julio-DelRosario <150268092+Julio-DelRosario@users.noreply.github.com>
2025-01-13 22:26:09 +0800

    Added product variable

--- js/products.js
+++ js/products.js
@@ -1,8 +1,12 @@
 var cart = [];
-
-var white_sage = { id: 1, name: "White Sage with Lavender and Palo Santo", price: 500 };
-var blue_sage = { id: 2, name: "Blue Sage", price: 300 };
-var cedar = { id: 3, name: "Cedar", price: 300 };
+var products = {
+    white_sage: { id: 1, name: "White Sage with Lavender and Palo Santo", price: 500 },
+    blue_sage: { id: 2, name: "Blue Sage", price: 300 },
+    cedar: { id: 3, name: "Cedar", price: 300 },
+    dragons_blood: { id: 4, name: "Dragon's Blood", price: 400 },
+    mugwort: { id: 5, name: "Mugwort", price: 400 },
+    yerba_santa: { id: 6, name: "Yerba Santa", price: 400 }
+};
 
 function addProduct(item){
     cart.push(item);
